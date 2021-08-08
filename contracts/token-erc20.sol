328c4245168e64221e2d1ba6f1f2aff18bee7308
George Paulose <georgepaulose@everledger.io>
2021-08-08 11:51:41 +0100

    Added functionality for token-erc20 and token-erc721

--- contracts/token-erc20.sol
+++ contracts/token-erc20.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-abstract contract TokenERC20 is ERC20, Ownable{
+contract TokenERC20 is ERC20, Ownable{
     constructor() ERC20("TEST", "TST") {}
+
+    function mint(uint256 amount) external{
+        _mint(msg.sender, amount);
+    }
 }
--- contracts/token-erc721.sol
+++ contracts/token-erc721.sol
@@ -3,7 +3,18 @@ pragma solidity ^0.8.0;
 // import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-abstract contract TokenERC721 is ERC721, Ownable{
+contract TokenERC721 is ERC721, Ownable{
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
     constructor() ERC721("TEST", "TST") {}
+
+    function mint() external returns (uint256)
+    { 
+         _tokenIds.increment();
+        _mint(msg.sender, _tokenIds.current());
+        return _tokenIds.current();
+    }
 }
